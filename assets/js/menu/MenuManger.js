d2f38afaa4d94f34f18ca0842f275c9318825129
nathanelmer <nelmer12@gmail.com>
2022-03-25 10:33:25 -0500

    categories

--- assets/js/menu/MenuManger.js
+++ assets/js/menu/MenuManger.js
@@ -4,3 +4,27 @@ export const getFoods = () => {
     return fetch(`${apiURL}/foods`)
     .then(response => response.json())
 }
+
+export const getApps = () => {
+    return fetch(`${apiURL}/foods`)
+    .then(res => res.json())
+    .then(data => data.filter(item => item.menuId === 3))
+}
+
+export const getLunch = () => {
+    return fetch(`${apiURL}/foods`)
+    .then(res => res.json())
+    .then(data => data.filter(item => item.menuId === 1))
+}
+
+export const getDesserts = () => {
+    return fetch(`${apiURL}/foods`)
+    .then(res => res.json())
+    .then(data => data.filter(item => item.menuId === 4))
+}
+
+export const getDrinks = () => {
+    return fetch(`${apiURL}/foods`)
+    .then(res => res.json())
+    .then(data => data.filter(item => item.menuId === 2))
+}
