e5e967e8ed04dd43ca4e76d36c93a83dabc54264
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-03-25 11:39:51 -0500

    sorting by price

--- assets/js/menu/MenuManger.js
+++ assets/js/menu/MenuManger.js
@@ -2,29 +2,45 @@ const apiURL = "http://localhost:8088";
 
 export const getFoods = () => {
     return fetch(`${apiURL}/foods`)
-    .then(response => response.json())
+        .then(response => response.json())
 }
 
 export const getApps = () => {
     return fetch(`${apiURL}/foods`)
-    .then(res => res.json())
-    .then(data => data.filter(item => item.menuId === 3))
+        .then(res => res.json())
+        .then(data => data.filter(item => item.menuId === 3))
+        .then(app => app.sort(function (a, b) {
+            return a.price - b.price
+            
+        }))
 }
 
 export const getLunch = () => {
     return fetch(`${apiURL}/foods`)
-    .then(res => res.json())
-    .then(data => data.filter(item => item.menuId === 1))
+        .then(res => res.json())
+        .then(data => data.filter(item => item.menuId === 1))
+        .then(lunch => lunch.sort(function (a, b) {
+            return a.price - b.price
+            
+        }))
 }
 
 export const getDesserts = () => {
     return fetch(`${apiURL}/foods`)
-    .then(res => res.json())
-    .then(data => data.filter(item => item.menuId === 4))
+        .then(res => res.json())
+        .then(data => data.filter(item => item.menuId === 4))
+        .then(dessert => dessert.sort(function (a, b) {
+            return a.price - b.price
+            
+        }))
 }
 
 export const getDrinks = () => {
     return fetch(`${apiURL}/foods`)
-    .then(res => res.json())
-    .then(data => data.filter(item => item.menuId === 2))
+        .then(res => res.json())
+        .then(data => data.filter(item => item.menuId === 2))
+        .then(drink => drink.sort(function (a, b) {
+            return a.price - b.price
+            
+        }))
 }
